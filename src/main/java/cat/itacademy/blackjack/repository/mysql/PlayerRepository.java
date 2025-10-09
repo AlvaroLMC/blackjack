64672074ed14710c65d33fc719ba985682096f4e
Alvaro Mendoza <alvaromendoxxa@gmail.com>
2025-10-09 10:07:17 +0200

    v4

--- src/main/java/cat/itacademy/blackjack/repository/mysql/PlayerRepository.java
+++ src/main/java/cat/itacademy/blackjack/repository/mysql/PlayerRepository.java
@@ -1,6 +1,7 @@
 package cat.itacademy.blackjack.repository.mysql;
 
 import cat.itacademy.blackjack.model.Player;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -9,5 +10,7 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface PlayerRepository extends R2dbcRepository<Player, Long> {
     Mono<Player> findByName(String name);
+
+    @Query("SELECT * FROM player ORDER BY player_wins_counter DESC")
     Flux<Player> findAllByOrderByPlayerWinsCounterDesc();
 }
